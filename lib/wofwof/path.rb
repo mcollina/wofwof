55d67ad56938d48c2f97bed48dd1b8aff69eaa02
Matteo Collina <matteocollina@gmail.com>
2010-01-20 13:37:40 +0100

    Little algorithm semplification of Path#route_to.

--- lib/wofwof/path.rb
+++ lib/wofwof/path.rb
@@ -65,25 +65,19 @@ module WofWof
       common = []
 
       self_local_path_splitted.each_with_index do |e, i|
-        #puts "self_local_path_splitted: #{e}"
-        #puts "other_local_path_splitted: #{other_local_path_splitted[i]}" if i < other_local_path_splitted.size
         if i < other_local_path_splitted.size and other_local_path_splitted[i] == e
           common << e
         else
           break
         end
       end
-
-      self_local_path_splitted -= common
-      other_local_path_splitted -= common
-
+      
       dest = []
 
-      go_back = self_local_path_splitted.size
-      go_back -= 1 unless go_back == 0
-
+      go_back = self_local_path_splitted.size - common.size - 1
       go_back.times { |i| dest << ".." }
-      
+
+      other_local_path_splitted -= common
       other_local_path_splitted.each { |e| dest << e }
 
       dest.join("/")
