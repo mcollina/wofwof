1191a0a5dfa9ea74251807ac53de021af84b02ff
Matteo Collina <matteo.collina@gmail.com>
2010-02-05 19:53:34 +0100

    Added the route_to filter to LiquidTemplateNode.

--- lib/wofwof/liquid_template_node.rb
+++ lib/wofwof/liquid_template_node.rb
@@ -2,14 +2,25 @@ require 'liquid'
 
 module WofWof
   class LiquidTemplateNode < Node
-    def initialize(source_path, content, meta_info={})
+    
+    def initialize(node_repository, source_path, content, meta_info={})
       super(source_path, meta_info)
       self.template = true
       @liquid_template = Liquid::Template.parse(content)
+      @node_repository = node_repository
     end
 
-    def render(hash)
-      @liquid_template.render(hash)
+    def render(node, hash={})
+      registers = { :current_node => node, :node_repository => @node_repository}
+      @liquid_template.render(hash, :filters => [RouteToFilter], :registers => registers)
+    end
+
+    module RouteToFilter
+      def route_to(input)
+        regexp = Regexp.new(input)
+        dest_node = @context.registers[:node_repository].find { |node| node.source_path =~ regexp }
+        @context.registers[:current_node].dest_path.route_to(dest_node.dest_path)
+      end
     end
   end
 end
--- spec/wofwof/liquid_template_node_spec.rb
+++ spec/wofwof/liquid_template_node_spec.rb
@@ -3,16 +3,50 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'shared_spec_node'))
 
 describe LiquidTemplateNode do
 
-  it_should_behave_like AllNodes
+  def build(content)
+    return LiquidTemplateNode.new(@node_repository, @source_path, content)
+  end
 
   before(:each) do
+    @node_repository = mock "NodeRepository"
     @source_path = mock "SourcePath"
-    @content = "Hello {{ name }}"
-    @instance = LiquidTemplateNode.new(@source_path, @content)
+    @node = mock "Node"
+    @node_dest_path = mock "Node Path"
+    @node.should_receive(:dest_path).any_number_of_times.and_return(@node_dest_path)
   end
 
   it "should render corretly the passed content" do
-    @instance.render("name" => "World").should == "Hello World"
+    instance = build("Hello {{ name }}")
+    instance.render(@node, "name" => "World").should == "Hello World"
+  end
+
+  it "should have a route_to filter" do
+    wrong_node = mock "Wrong Node"
+    wrong_path = mock "Wrong path"
+    wrong_node.should_receive(:source_path).and_return(wrong_path)
+    wrong_path.should_receive(:=~).with(/a_node/).and_return false
+
+    right_node = mock "Right Node"
+    right_source_path = mock "Right source path"
+    right_node.should_receive(:source_path).and_return(right_source_path)
+    right_source_path.should_receive(:=~).with(/a_node/).and_return true
+    right_dest_path = mock "Right dest path"
+    right_node.should_receive(:dest_path).and_return(right_dest_path)
+
+    @node_dest_path.should_receive(:route_to).with(right_dest_path).and_return("path/to/heaven")
+
+    @node_repository.should_receive(:find).and_yield(wrong_node).and_yield(right_node).and_return(right_node)
+    instance = build("{{ 'a_node' | route_to }}")
+    instance.render(@node).should == "path/to/heaven"
+  end
+
+  describe "as a generic node" do
+
+    it_should_behave_like AllNodes
+
+    before(:each) do
+      @instance = build("Hello {{ name }}")
+    end
   end
 end
 
