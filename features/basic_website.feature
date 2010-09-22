7ed2716923ca745e2ef31fc2467d13e44a49570f
Matteo Collina <matteo.collina@gmail.com>
2010-09-22 14:14:56 +0200

    Implemented a basic cucumber feature.

--- features/basic_website.feature
+++ features/basic_website.feature
@@ -1,10 +1,11 @@
 Feature: User creates a basic website 
+  As a User
   In order to create a basic website
 
   Scenario: build a single page website 
     Given a new project 
-    And the liquid template folder features/basic_website/templates/
-    And the pages folder features/basic_website/pages
-    When I build the website
+    And the liquid template folder basic_website/templates/
+    And the pages folder basic_website/pages
+    When I render the website
     Then everyone should see that there is an index.html file
-    And that the index.html file contains the text in features/basic_website/pages/index.page
+    And that the index.html file contains the text in basic_website/pages/index.page
--- features/step_definitions/wofwof_steps.rb
+++ features/step_definitions/wofwof_steps.rb
@@ -1,24 +1,34 @@
+require 'tmpdir'
 
 Given /a new project/ do
-  pending  
+  @website_dest = Dir.mktmpdir("wofwof") # TODO add the scenario name in the feature?
+  @runtime = WofWof::Runtime.new
+  @runtime.dest_path_handler = WofWof::FileSystemPathHandler.new(WofWof::Path.new(@website_dest, ""))
+  #Kernel.at_exit { Dir.rmdir @website_dest } # TODO add a way to skip deletion
 end
 
-Given /^the liquid template folder features\/basic_website\/templates\/$/ do
-  pending # express the regexp above with the code you wish you had
+Given /^the liquid template folder (.*)$/ do |path|
+  @runtime.sources << WofWof::LiquidTemplateSource.new(WofWof::FileSystemPathHandler.new(WofWof::Path.new(
+    File.join(File.dirname(__FILE__), "../", path), "")))
 end
 
-Given /^the pages folder features\/basic_website\/pages$/ do
-  pending # express the regexp above with the code you wish you had
+Given /^the pages folder (.*)$/ do |path|
+  @runtime.sources << WofWof::PageSource.new(WofWof::FileSystemPathHandler.new(WofWof::Path.new(
+    File.join(File.dirname(__FILE__), "../", path), "")))
 end
 
-When /^I build the website$/ do
-  pending # express the regexp above with the code you wish you had
+When /^I render the website$/ do
+  @runtime.render
 end
 
-Then /^everyone should see that there is an index\.html file$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^everyone should see that there is an (.*) file$/ do |file|
+  File.should be_exists(File.join(@website_dest, file))
 end
 
-Then /^that the index\.html file contains the text in features\/basic_website\/pages\/index\.page$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^that the (.*) file contains the text in (.*)$/ do |destination, source|
+  open(File.join(@website_dest,destination), "r") do |dest_io|
+    open(File.join(File.dirname(__FILE__), "../", source), "r") do |source_io|
+      dest_io.read.should =~ /#{source_io.read}/
+    end
+  end
 end
