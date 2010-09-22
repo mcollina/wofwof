8183d8250d4c00807df5aefd1325bc0c51789d0c
Matteo Collina <matteo.collina@gmail.com>
2010-09-22 16:05:25 +0200

    Extracted the parsing logic from the PageNode initializer.

--- lib/wofwof/page_node.rb
+++ lib/wofwof/page_node.rb
@@ -11,46 +11,53 @@ module WofWof
       self.dest_path = source_path.change_ext("html")
       
       @node_repository = node_repository 
-      @content = {}
-
+      
       path_handler.open(source_path, "r") do |io|
-        
-        content = io.read
-
-        split = content.split(/^---/)
-       
-        if split.size == 1
-          @content[:main] = content
-        else
-          meta = YAML.load(split[0])
-          if meta.respond_to? :each_pair
-            # it's an hash, so we can inject its content in the meta_info hash
-            meta.each_pair { |key, value| meta_info[key.to_sym] = value }
-            split.shift
-          end
-          
-          split.each do |section|
-            section_split = section.split("\n")
-            section_content = section_split[1..-1].join("\n") + "\n"
-            
-            if section_split[0].strip == ""
-              section_name = :main
-            elsif section_content.strip == ""
-              section_name = :main
-              section_content = split[0]
-            else
-              section_name = section_split[0].strip.to_sym
-            end
-            
-            @content[section_name] = section_content
-          end
-        end 
+        @content, meta_info = PageNode.parse(io)
+        self.meta_info.merge! meta_info 
       end
-
     end
 
     def build(io)
       io << template.render(self, meta_info.dup.merge!(content))
+    end
+
+    def self.parse(io)        
+      content = io.read
+
+      split = content.split(/^---/)
+    
+      content_hash = {}
+      meta_info = {} 
+       
+      if split.size == 1
+        content_hash[:main] = content
+      else
+        meta = YAML.load(split[0])
+        if meta.respond_to? :each_pair
+          # it's an hash, so we can inject its content in the meta_info hash
+          meta.each_pair { |key, value| meta_info[key.to_sym] = value }
+          split.shift
+        end
+        
+        split.each do |section|
+          section_split = section.split("\n")
+          section_content = section_split[1..-1].join("\n") + "\n"
+          
+          if section_split[0].strip == ""
+            section_name = :main
+          elsif section_content.strip == ""
+            section_name = :main
+            section_content = split[0]
+          else
+            section_name = section_split[0].strip.to_sym
+          end
+          
+          content_hash[section_name] = section_content
+        end
+      end
+
+      [content_hash, meta_info] 
     end
 
     private
