5223e28f343d43a11ec81485ed72781e989c6dd1
Matteo Collina <matteo.collina@gmail.com>
2010-09-23 12:29:40 +0200

    Added the ability to store everything to ConfigurationStore.

--- lib/wofwof/configuration_store.rb
+++ lib/wofwof/configuration_store.rb
@@ -6,6 +6,7 @@ module WofWof
 
     def initialize(node_repository)
       @node_repository = node_repository
+      @store = {}
     end
 
     def default_template=(template)
@@ -28,6 +29,24 @@ module WofWof
       end
 
       templates.first
+    end
+
+    def respond_to?(method)
+      if @store.has_key? method.to_sym or @store.has_key? method.to_s.gsub(/[=\?]$/, "").to_sym
+        return true 
+      end
+      super
+    end
+
+    private
+    def method_missing(name, *args, &block)
+      if name.to_s =~ /=$/ 
+        @store[name.to_s.gsub(/=$/, "").to_sym] = args.first
+        return args.first
+      else
+        raise "the #{name.to_s} method accept zero arguments" if args.size != 0 
+        return @store[name.to_s.gsub(/\?$/, "").to_sym]
+      end
     end
   end
 end
--- spec/wofwof/configuration_store_spec.rb
+++ spec/wofwof/configuration_store_spec.rb
@@ -47,4 +47,34 @@ describe ConfigurationStore do
     @node_repository.should_receive(:store).with(node)
     @instance.default_template = node
   end
+
+  it "should be able to contain elements that weren't specified" do
+    lambda { 
+      @instance.hello = :world
+      @instance.hello.should == :world
+    }.should_not raise_error
+  end
+
+  it "should raise exception if we call read methods with arguments" do
+    lambda {
+      @instance.hello :world
+    }.should raise_error
+  end
+  
+  it "should be able to contain boolean elements that weren't specified" do
+    lambda { 
+      @instance.hello = true 
+      @instance.should be_hello
+    }.should_not raise_error
+  end
+
+  it "should have a respond_to method that returns true after a value has been set" do
+    @instance.should_not respond_to(:quack)
+    @instance.should_not respond_to(:quack=)
+    @instance.should_not respond_to(:quack?)
+    @instance.quack = :quack
+    @instance.should respond_to(:quack)
+    @instance.should respond_to(:quack=)
+    @instance.should respond_to(:quack?)
+  end
 end
